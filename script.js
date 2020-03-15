88c715da15e05b26a4e2a707104eb9f9ced400ef
gamerDraux <61572578+GamerDraux@users.noreply.github.com>
2020-03-14 21:15:48 -0500

    completed basic single die roll page with reset button

--- script.js
+++ script.js
@@ -19,23 +19,45 @@ function initalize (){
     console.log (d12button);
     d20button=document.getElementById("d20button");
     console.log (d20button);
+    resetButton=document.getElementById("reset");
+    console.log (resetButton);
+
+    resetButton.addEventListener('click', function(){
+        let elements=document.getElementsByClassName("results");
+        for (i=0; i<elements.length; i++){
+            elements[i].style.backgroundColor="grey";
+            elements[i].innerHTML=""
+        }
+    })
     d4button.addEventListener('click', function(event) {
         document.getElementById("d4result").innerHTML=(singleRoll(4));
+        document.getElementById("d4result").style.backgroundColor='black';
+        document.getElementById("d4result").style.color="silver";
     });
     d6button.addEventListener('click', function(event){
         document.getElementById("d6result").innerHTML=(singleRoll(6));
+        document.getElementById('d6result').style.backgroundColor='black';
+        document.getElementById('d6result').style.color='silver';
     });
     d8button.addEventListener('click', function(event) {
         document.getElementById("d8result").innerHTML=(singleRoll(8));
+        document.getElementById('d8result').style.backgroundColor='black';
+        document.getElementById('d8result').style.color='silver';
     });
     d10button.addEventListener('click', function(event) {
         document.getElementById("d10result").innerHTML=(singleRoll(10));
+        document.getElementById("d10result").style.backgroundColor='black';
+        document.getElementById("d10result").style.color='silver';
     });
     d12button.addEventListener('click', function(event) {
         document.getElementById("d12result").innerHTML=(singleRoll(12));
+        document.getElementById("d12result").style.backgroundColor='black';
+        document.getElementById("d12result").style.color='silver';    
     });
     d20button.addEventListener('click', function(event) {
         document.getElementById("d20result").innerHTML=(singleRoll(20));
+        document.getElementById("d20result").style.backgroundColor='black';
+        document.getElementById("d20result").style.color='silver';
     });
 }
  
